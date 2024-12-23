0360caca924bd1bedccc102c2f06cc8413eb93ef
rahgun04 <38143512+rahgun04@users.noreply.github.com>
2024-12-23 12:27:05 +0000

    CMake Changes

--- out/build/x64-debug/VulkanDevelopment/cmake_install.cmake
+++ out/build/x64-debug/VulkanDevelopment/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Users/rahul/Source/repos/VulkanDevelopment/VulkanDevelopment
+# Install script for directory: C:/Users/rahul/source/repos/rahgun04/VulkanDevelopment/VulkanDevelopment
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/Users/rahul/Source/repos/VulkanDevelopment/out/install/x64-debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/rahul/Source/Repos/rahgun04/VulkanDevelopment/out/install/x64-debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- out/build/x64-debug/cmake_install.cmake
+++ out/build/x64-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Users/rahul/Source/repos/VulkanDevelopment
+# Install script for directory: C:/Users/rahul/source/repos/rahgun04/VulkanDevelopment
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/Users/rahul/Source/repos/VulkanDevelopment/out/install/x64-debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/rahul/Source/Repos/rahgun04/VulkanDevelopment/out/install/x64-debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,32 +34,32 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/Users/rahul/Source/repos/VulkanDevelopment/out/build/x64-debug/VulkanDevelopment/cmake_install.cmake")
+  include("C:/Users/rahul/source/repos/rahgun04/VulkanDevelopment/out/build/x64-debug/VulkanDevelopment/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/Users/rahul/Source/repos/VulkanDevelopment/out/build/x64-debug/src/cmake_install.cmake")
+  include("C:/Users/rahul/source/repos/rahgun04/VulkanDevelopment/out/build/x64-debug/src/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/Users/rahul/Source/repos/VulkanDevelopment/out/build/x64-debug/thirdParty/cmake_install.cmake")
+  include("C:/Users/rahul/source/repos/rahgun04/VulkanDevelopment/out/build/x64-debug/thirdParty/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/Users/rahul/Source/repos/VulkanDevelopment/out/build/x64-debug/vk-bootstrap/cmake_install.cmake")
+  include("C:/Users/rahul/source/repos/rahgun04/VulkanDevelopment/out/build/x64-debug/vk-bootstrap/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/Users/rahul/Source/repos/VulkanDevelopment/out/build/x64-debug/tinyobjloader/cmake_install.cmake")
+  include("C:/Users/rahul/source/repos/rahgun04/VulkanDevelopment/out/build/x64-debug/tinyobjloader/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/Users/rahul/Source/repos/VulkanDevelopment/out/build/x64-debug/glm/cmake_install.cmake")
+  include("C:/Users/rahul/source/repos/rahgun04/VulkanDevelopment/out/build/x64-debug/glm/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -70,5 +70,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Users/rahul/Source/repos/VulkanDevelopment/out/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/rahul/source/repos/rahgun04/VulkanDevelopment/out/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
